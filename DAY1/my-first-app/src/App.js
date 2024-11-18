1bc95bf5b4de0853518fdfeab8836d84ba4c715c
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-11-18 12:47:49 +0530

    ✨ Passing props as an object

--- DAY1/my-first-app/src/App.js
+++ DAY1/my-first-app/src/App.js
@@ -3,17 +3,33 @@ import React from "react";
 import Message from "./Message";
 
 class App extends React.Component {
+  messageOne = {
+    msg: "Hey",
+    from: "John",
+    to: "Carter",
+    imageUrl:
+      "https://t3.ftcdn.net/jpg/02/39/06/68/360_F_239066841_TGuyjQRJuQDDML5btQq9jQszX57cQUpk.jpg",
+  };
+  messageTwo = {
+    msg: "Hi",
+    from: "Peter",
+    to: "Parker",
+    imageUrl:
+      "https://www.shutterstock.com/shutterstock/photos/1138004576/display_1500/stock-vector-hi-sticker-for-social-media-content-vector-hand-drawn-illustration-design-bubble-pop-art-comic-1138004576.jpg",
+  };
+  messageThree = {
+    msg: "Bye",
+    from: "James",
+    to: "Bond",
+    imageUrl:
+      "https://media.gettyimages.com/id/1207131318/vector/bye-speech-bubble-vector-illustration-design-element.jpg?s=612x612&w=gi&k=20&c=pelaDlxU3ygaCU5xV_j3T08PAcEtVTBxwhTQ67BGcvo=",
+  };
   render() {
     return (
       <div>
-        <Message
-          msg="Hey"
-          from="John"
-          to="Carter"
-          imageUrl="https://t3.ftcdn.net/jpg/02/39/06/68/360_F_239066841_TGuyjQRJuQDDML5btQq9jQszX57cQUpk.jpg"
-        />
-        {/* <Message msg="Hi" from="Peter" to="Parker" />
-        <Message msg="Bye" from="James" to="Bond" /> */}
+        <Message messagedetails={this.messageOne} />
+        <Message messagedetails={this.messageTwo} />
+        <Message messagedetails={this.messageThree} />
       </div>
     );
   }
--- DAY1/my-first-app/src/Message.jsx
+++ DAY1/my-first-app/src/Message.jsx
@@ -4,10 +4,15 @@ export default class Message extends React.Component {
   render() {
     return (
       <div>
-        <img src={this.props.imageUrl} alt={this.props.msg} />
-        <h2> {this.props.msg} ! </h2>
-        <h3> From : {this.props.from}</h3>
-        <h3> To : {this.props.to}</h3>
+        <img
+          src={this.props.messagedetails.imageUrl}
+          alt={this.props.messagedetails.msg}
+          height={100}
+          width={200}
+        />
+        <h2> {this.props.messagedetails.msg} ! </h2>
+        <h3> From : {this.props.messagedetails.from}</h3>
+        <h3> To : {this.props.messagedetails.to}</h3>
       </div>
     );
   }
