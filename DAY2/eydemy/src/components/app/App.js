4934026165b106b81ea98fce8b7fe60b2a5adcd5
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-11-21 11:26:49 +0530

    ✨ Adding a navbar

--- DAY2/eydemy/src/components/app/App.js
+++ DAY2/eydemy/src/components/app/App.js
@@ -5,14 +5,17 @@ import Posts from "../posts/posts";
 import GetPostById from "../getpostbyid/getpostbyid";
 import NewCourse from "../newcourse/newcourse";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Navbar from "../navbar/navbar";
 
 const App = () => (
   <BrowserRouter>
-    <Link to="/">Courses</Link> | <Link to="/posts">Posts</Link>
+    {/* <Link to="/">Courses</Link> | <Link to="/posts">Posts</Link> */}
+    <Navbar />
     <Routes>
       <Route path="/" Component={ListOfCourses} />
       <Route path="/posts" Component={Posts} />
       <Route path="/postbyid" Component={GetPostById} />
+      <Route path="/newcourse" Component={NewCourse} />
     </Routes>
   </BrowserRouter>
 );
--- DAY2/eydemy/src/components/listofcourses/listofcourses.jsx
+++ DAY2/eydemy/src/components/listofcourses/listofcourses.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Course from "../course/course";
 import axios from "axios";
-import NewCourse from "../newcourse/newcourse";
 
 export default function ListOfCourses() {
   const [courses, setCourses] = useState([]);
