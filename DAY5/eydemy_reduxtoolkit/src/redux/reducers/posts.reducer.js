7043ee3b52537bb22e02650ffcced98df0b962f8
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-11-22 13:10:10 +0530

    ✨ Adding Reducers & Store

---
+++ DAY5/eydemy_reduxtoolkit/src/redux/reducers/posts.reducer.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let initialState = [
+  {
+    id: "1",
+    title:
+      "React is a free and open-source front-end JavaScript library for building user interfaces based on components. ",
+  },
+];
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    addPost: (store, action) => {
+      return store; // updated store
+    },
+  },
+});
+
+export const { addPost } = postsSlice.actions;
+export default postsSlice.reducer;
