efe2e59a882fa6ebcb20da0f2d96219967e2bcfb
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-11-28 11:54:11 +0530

    ✨ Adding users as seperate model

---
+++ DAY9/GraphQL/server/src/model/user.js
@@ -0,0 +1,20 @@
+export let usersModel = [
+  {
+    id: 1,
+    name: "Jack Well",
+    email: "jack@well.com",
+    age: 30,
+  },
+  {
+    id: 2,
+    name: "Jack Phels",
+    email: "jack@phels.com",
+    age: 50,
+  },
+  {
+    id: 3,
+    name: "Jack Jofer",
+    email: "jack@jofer.com",
+    age: 40,
+  },
+];
--- DAY9/GraphQL/server/src/schema/resolvers.js
+++ DAY9/GraphQL/server/src/schema/resolvers.js
@@ -1,23 +1,4 @@
-let usersModel = [
-  {
-    id: 1,
-    name: "Jack Well",
-    email: "jack@well.com",
-    age: 30,
-  },
-  {
-    id: 2,
-    name: "Jack Phels",
-    email: "jack@phels.com",
-    age: 50,
-  },
-  {
-    id: 3,
-    name: "Jack Jofer",
-    email: "jack@jofer.com",
-    age: 40,
-  },
-];
+import { usersModel } from "../model/user.js";
 
 const resolvers = {
   Query: {
