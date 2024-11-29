9aba61e98a051f98a2b694d0a9111141348226a5
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-11-29 10:22:17 +0530

    ✨ using Apollo Client (client.query)

---
+++ DAY9/GraphQL/client_apollo_client/src/components/app/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h2> Using Apollo Client</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ DAY9/GraphQL/client_apollo_client/src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/app/App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetAllCourses {
+        courses {
+          title
+          price
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+);
